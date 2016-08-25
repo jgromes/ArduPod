8943fbb0d0cbb10d92192a6ef30299aef3bd54aa
Gipsonek <jan.gromes@gmail.com>
2016-08-25 08:50:49 +0200

    Update AP_Utils

--- arduino/AP_Utils/AP_Utils.cpp
+++ arduino/AP_Utils/AP_Utils.cpp
@@ -1,27 +1,39 @@
 #include <AP_Utils.h>
 
 AP_Utils::AP_Utils(void) {
- reset();
+  for(int i=0; i<16; i++) {
+    servos[i].number = i;
+    if(i % 2 == 0) {
+      servos[i].type = HORIZONTAL;
+    } else {
+      servos[i].type = VERTICAL;
+    }
+  }
+  pwm.begin();
+  pwm.setPWMFreq(60);
+  pwm.setPWM(15, 0, pulseLength(90));
+  reset();
 }
 
 void AP_Utils::reset(void) {
- for(int i=0; i<11; i++) {
-  pwm.setPWM(i, 0, pulseLength(90));
- }
+  for(int i=0; i<16; i++) {
+    pwm.setPWM(i, 0, pulseLength(90));
+    servos[i].position = 90;
+  }
 }
 
 void AP_Utils::stretchAll(void) {
- for(int i=0; i<2; i++) {
-  legStretch(0, true);
-  legStretch(2, true);
-  legStretch(4, true);
-  delay(100);
+  for(int i=0; i<2; i++) {
+    legStretch(0, true);
+    legStretch(2, true);
+    legStretch(4, true);
+    delay(DELAY);
 
-  legStretch(1, true);
-  legStretch(3, true);
-  legStretch(5, true);
-  delay(100);
- }
+    legStretch(1, true);
+    legStretch(3, true);
+    legStretch(5, true);
+    delay(DELAY);
+  }
 }
 
 void AP_Utils::walk(int dir) {
@@ -29,158 +41,79 @@ void AP_Utils::walk(int dir) {
 }
 
 int AP_Utils::pulseLength(int deg) {
- return map(deg, 0, 180, 0, 4095);
+  return map(deg, 0, 180, SERVOMIN, SERVOMAX);
+}
+
+void AP_Utils::pwmove(uint8_t i, int deg) {
+  pwm.setPWM(i, 0, pulseLength(deg));
+  servos[i].position = deg;
+  delay(DELAY);
 }
 
 void AP_Utils::legUp(uint8_t leg, bool smooth) {
- uint8_t servo = vertical[leg];
- //pwm.setPWM(servo, 0, pulseLength(VERT_MAX));
- moveServo(servo, pulseLength(VERT_MAX), smooth);
- #ifdef DEBUG
-  Serial.print("[INF]Leg #");
-  Serial.print(leg);
-  Serial.print(" up ");
-  if(smooth) {
-   Serial.println("(smooth)");
-  }
- #endif
+  uint8_t servo = vertical[leg];
+  //pwm.setPWM(servo, 0, pulseLength(VERT_MAX));
+  moveServo(servo, pulseLength(VERT_MAX), smooth);
+  #ifdef DEBUG
+    Serial.print("[INF]Leg #");
+    Serial.print(leg);
+    Serial.print(" up ");
+    if(smooth) {
+      Serial.println("(smooth)");
+    }
+  #endif
 }
 
 void AP_Utils::legDown(uint8_t leg, bool smooth) {
- uint8_t servo = vertical[leg];
- //pwm.setPWM(servo, 0, pulseLength(VERT_MIN));
- moveServo(servo, pulseLength(VERT_MIN), smooth);
- #ifdef DEBUG
-  Serial.print("[INF]Leg #");
-  Serial.print(leg);
-  Serial.print(" down ");
-  if(smooth) {
-   Serial.println("(smooth)");
-  }
- #endif
+  uint8_t servo = vertical[leg];
+  //pwm.setPWM(servo, 0, pulseLength(VERT_MIN));
+  moveServo(servo, pulseLength(VERT_MIN), smooth);
+  #ifdef DEBUG
+    Serial.print("[INF]Leg #");
+    Serial.print(leg);
+    Serial.print(" down ");
+    if(smooth) {
+      Serial.println("(smooth)");
+    }
+  #endif
 }
 
-void AP_Utils::moveServo(uint8_t servo, int deg, bool smooth) {
- if(smooth) {
-  if(servo % 2 == 0) {
-   //horizontal servos
-   if((deg >= HORIZ_MIN) || (deg <= HORIZ_MAX)) {
-    for(int i=0; i<=deg; i++) {
-     pwm.setPWM(servo, 0, pulseLength(i));
-     delay((cos(i*(PI/90)) + 1)*7.5);
+void AP_Utils::moveServo(uint8_t number, int deg, bool smooth) {
+  servo moving = servos[number];
+  if(smooth) {
+    if(moving.type == HORIZONTAL) {
+      if(moving.position > deg) {
+        for(int i=moving.position; i<=deg; i++) {
+          pwm.setPWM(moving.number, 0, pulseLength(i));
+          delay((cos(i*(PI/90)) + 1)*5);
+        }
+      } else if(moving.position < deg) {
+        for(int i=moving.position; i>=deg; i--) {
+          pwm.setPWM(moving.position, 0, pulseLength(i));
+          delay((cos(i*(PI/90)) + 1)*5);
+        }
+      }
+    } else if(moving.type == VERTICAL) {
+      if(moving.position > deg) {
+        for(int i=moving.position; i<=deg; i++) {
+          pwm.setPWM(moving.number, 0, pulseLength(i));
+          delay((cos(i*(PI/90)) + 1)*5);
+        }
+      } else if(moving.position < deg) {
+        for(int i=moving.position; i>=deg; i--) {
+          pwm.setPWM(moving.number, 0, pulseLength(i));
+          delay((cos(i*(PI/90)) + 1)*5);
+        }
+      }
     }
-   } else {
-    //out of bounds!
-    #ifdef DEBUG
-     int bound;
-     if(deg >= HORIZ_MAX) {
-      bound = HORIZ_MAX;
-     } else {
-      bound = HORIZ_MIN;
-     }
-     Serial.print("[ERR]Servo #");
-     Serial.print(servo);
-     Serial.println(" out of bounds!");
-    
-     Serial.print("     Command sent : ");
-     Serial.print(deg);
-     Serial.println(" deg");
-
-     Serial.print("     Current bound: ");
-     Serial.print(bound);
-     Serial.println(" deg");
-    #endif
-   }
   } else {
-   //vertical servos
-   if((deg >= VERT_MIN) || (deg <= VERT_MAX)) {
-    for(int i=0; i<=deg; i++) {
-     pwm.setPWM(servo, 0, pulseLength(i));
-     delay((cos(i*(PI/90)) + 1)*7.5);
-    }
-   } else {
-    //out of bounds!
-    #ifdef DEBUG
-     int bound;
-     if(deg >= VERT_MAX) {
-      bound = VERT_MAX;
-     } else {
-      bound = VERT_MIN;
-     }
-     Serial.print("[ERR]Servo #");
-     Serial.print(servo);
-     Serial.println(" out of bounds!");
-    
-     Serial.print("     Command sent : ");
-     Serial.print(deg);
-     Serial.println(" deg");
-
-     Serial.print("     Current bound: ");
-     Serial.print(bound);
-     Serial.println(" deg");
-    #endif
-   }
+    pwmove(number, deg);
   }
- } else {
-  if(servo % 2 == 0) {
-   //horizontal servos
-   if((deg >= HORIZ_MIN) || (deg <= HORIZ_MAX)) {
-    pwm.setPWM(servo, 0, pulseLength(deg));
-   } else {
-    //out of bounds!
-    #ifdef DEBUG
-     int bound;
-     if(deg >= HORIZ_MAX) {
-      bound = HORIZ_MAX;
-     } else {
-      bound = HORIZ_MIN;
-     }
-     Serial.print("[ERR]Servo #");
-     Serial.print(servo);
-     Serial.println(" out of bounds!");
-    
-     Serial.print("     Command sent : ");
-     Serial.print(deg);
-     Serial.println(" deg");
-
-     Serial.print("     Current bound: ");
-     Serial.print(bound);
-     Serial.println(" deg");
-    #endif
-   }
-  } else {
-   //vertical servos
-   if((deg >= VERT_MIN) || (deg <= VERT_MAX)) {
-    pwm.setPWM(servo, 0, pulseLength(deg));
-   } else {
-    //out of bounds!
-    #ifdef DEBUG
-     int bound;
-     if(deg >= VERT_MAX) {
-      bound = VERT_MAX;
-     } else {
-      bound = VERT_MIN;
-     }
-     Serial.print("[ERR]Servo #");
-     Serial.print(servo);
-     Serial.println(" out of bounds!");
-    
-     Serial.print("     Command sent : ");
-     Serial.print(deg);
-     Serial.println(" deg");
-
-     Serial.print("     Current bound: ");
-     Serial.print(bound);
-     Serial.println(" deg");
-    #endif
-   }
-  }
- }
 }
 
 void AP_Utils::legStretch(uint8_t leg, bool smooth) {
- legUp(leg, smooth);
- delay(50);
- legDown(leg, smooth);
- delay(50);
+  legUp(leg, smooth);
+  delay(50);
+  legDown(leg, smooth);
+  delay(50);
 }
--- arduino/AP_Utils/AP_Utils.h
+++ arduino/AP_Utils/AP_Utils.h
@@ -7,13 +7,15 @@
  #include "WProgram.h"
 #endif
 
-#include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
 //#define DEBUG
 
 #define SERVOMIN    150
-#define SERVOMAX    600
+#define SERVOMAX    560
+
+#define HORIZONTAL  0
+#define VERTICAL    1
 
 #define HORIZ_MAX   50
 #define HORIZ_DEF   90
@@ -22,9 +24,20 @@
 #define VERT_DEF    90
 #define VERT_MIN    130
 
+#define DELAY       100
+
+struct servo {
+  uint8_t number;
+  uint8_t type;
+  int position;
+};
+
 class AP_Utils {
  public:
   AP_Utils(void);
+  Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+  servo servos[16];
+  void moveServo(uint8_t servo, int deg, bool smooth);
   void reset(void);
   void stretchAll(void);
   void walk(int dir);
@@ -32,14 +45,14 @@ class AP_Utils {
   //uint8_t _horiz_max, _horiz_def, _horiz_min, _vert_max, _vert_def, _vert_min;
   uint8_t horizontal[6] = {0, 2, 4, 6, 8, 10};
   uint8_t vertical[6] = {1, 3, 5, 7, 9, 11};
-  Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
   int pulseLength(int deg);
   void legUp(uint8_t leg, bool smooth);
   void legDown(uint8_t leg, bool smooth);
   void legStretch(uint8_t leg, bool smooth);
+  void pwmove(uint8_t i, int deg);
 
-  void moveServo(uint8_t servo, int deg, bool smooth);
+  
   //void smoothMove(uint8_t servo, int deg);
 };
 
