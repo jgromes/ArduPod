6dc6bd709de693e7d09e13b414bdef425f2566c1
Gipsonek <jan.gromes@gmail.com>
2016-09-14 18:43:45 +0200

    Added calibration and servo_test examples

---
+++ arduino/AP_Utils/examples/calibration/calibration.ino
@@ -0,0 +1,30 @@
+/*
+ * ArduPod calibration sketch
+ * 
+ * Used to calibrate servo offsets so the servos 90 degrees position corresponds with the true middle position.
+ * It is highly recommended to run this example and calibrate the servos you will be using.
+ * 
+ * Calibration guide:
+ * The 'offsets' array stores the offsets (in degrees). Usually, this number is between -10 and 10.
+ * If you think you need to offset the servo more than that, you should try to unscrew the servo from the axis and move it.
+ * Start with 0 offset and increase/decrease the offset and upload again. Repeat until the servo is in the middle position.
+ * When you have all the servos in the middle, save the offsets. You can use these in any other sketches.
+ * Just initialize the array with the offsets you measured here before calling 'begin()' function in the 'setup()' routine,
+ * the same way it is done in servo_test.ino example.
+ * 
+ * Brought into existence and debugged out of it by Gipsonek, 2016
+ */
+ 
+#include <AP_Utils.h>
+
+//create AP_Utils object to access the functions
+AP_Utils ardupod;
+
+void setup() {}
+
+void loop() {
+  //initialize the offset array, you can change the numbers and observe the effect on the servos
+  int offsets[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  //start ArduPod with the offsets
+  ardupod.begin(offsets);
+}
---
+++ arduino/AP_Utils/examples/servo_test/servo_test.ino
@@ -0,0 +1,61 @@
+/*
+ * Servo test sketch
+ * 
+ * Used to calibrate the maximum and minimum boundaries for the servos.
+ * It is absolutely necessary to run this example and calibrate the servos you will be using.
+ * 
+ * Calibration guide:
+ * After uploading this example, the first servo (number 0) will sweep from the minimum to the maximum position.
+ * You have to check whether it is reaching its physical limit.
+ * The easiest way to do that is to disconnect power to the servo shield when the servo is in one of the limit positions.
+ * With disconnected power, try to move the servo further in the direction it was moving before.
+ * If it can still be moved, try to tweak the 'SERVOMIN' (or 'SERVOMAX') a little bit (about 10 at a time).
+ * Do NOT try to move the servo beyond its physical limit, this might destroy your servo!
+ * 
+ * Brought into existence and debugged out of it by Gipsonek, 2016
+ */
+
+#include <Adafruit_PWMServoDriver.h>
+
+//default SERVOMIN and SERVOMAX values
+//be extremely careful when changing these, only in small incremets (about 10) at a time
+//too big change (100 and more) can cause your servos gears to break!
+#define SERVOMIN 150
+#define SERVOMAX 560
+
+//create Adafruit PWM Servo Driver object to directly control the PWM outputs
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+void setup() {
+  Serial.begin(9600);
+  pwm.begin();
+  pwm.setPWMFreq(60);
+}
+
+void loop() {
+  //move first servo from middle to max position
+  for(int pulse=(SERVOMAX-SERVOMIN)/2; pulse<=SERVOMAX; pulse++) {
+    pwm.setPWM(15, 0, pulse);
+    Serial.println(pulse);
+    delay(10);
+  }
+  //wait 3 seconds to allow power to be unplugged
+  delay(3000);
+
+  //move from min to max and back forever
+  while(true) {
+    for(int pulse=SERVOMAX; pulse>=SERVOMIN; pulse--) {   
+      pwm.setPWM(15, 0, pulse);
+      Serial.println(pulse);
+      delay(10);
+    }
+    delay(3000);
+
+    for(int pulse=SERVOMIN; pulse<=SERVOMAX; pulse++) {   
+      pwm.setPWM(15, 0, pulse);
+      Serial.println(pulse);
+      delay(10);
+    }
+    delay(3000);
+  }
+}
